afd37a34806960bfe35eb407d61fb058220e1880
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-04-17 21:08:32 +0300

    added

---
+++ CarpimTablosuOgrenme.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	int num, i, result;
+	
+    printf("Bir tam sayi giriniz: ");
+    scanf("%d", &num);
+    
+    for (i=1; i<=10; ++i) {
+        printf("\n%d x %d = ", num, i, result);
+        scanf("%d", &result);
+        
+        if(result == (num * i)){
+			printf("\nHarika! Dogru bildiniz. Tebrikler :)\n");
+		}
+		else{
+			printf("\nYanlis cevap! Lutfen tekrar deneyiniz...");
+			break;
+		}  
+    }
+	
+	
+	return 0;
+	
+}
