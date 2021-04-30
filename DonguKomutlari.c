c9f90caaeced740b9eca6d981872a1fb7aebf0a9
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-04-30 04:34:55 +0300

    Update DonguKomutlari.c

--- DonguKomutlari.c
+++ DonguKomutlari.c
@@ -9,9 +9,12 @@ int main(int argc, char *argv[]) {
 	printf("2 tane pozitif tam sayi giriniz: ");													// kullanıcıdan 2 tane sayı girmesini istedik.
 	scanf("%d %d", &m, &n);
 	
-	
-	if(n < m){																						// 2. sayının 1. sayıdan büyük olması şartını belirledik.
+	if(n <= 0 || m <= 0){																			// kullanıcının negatif sayı girme olasılığına karşı durum belirledik.
+		printf("\nLutfen pozitif tam sayi giriniz...");												// olası durumlardan birini kontrol etmek için ekrana hata mesajı yazdırdık.
+	}
+	else if(n < m){																					// 2. sayının 1. sayıdan büyük olması şartını belirledik.
 		printf("\nHata! ilk girdiginiz sayi ikinci girdiginiz sayidan daha kucuk olmalidir.");
+		
 	}
 	else if(n > m){																					// 2. sayı 1. sayıdan büyük olduğunda çalışacak komutların şartını belirledik.
 		for(i=m; i<=n; i++){
@@ -26,18 +29,15 @@ int main(int argc, char *argv[]) {
 				average1 = result1 / oneNum;														// tek sayıların oratalamasını hesapladık.
 			}
 		}
-		
+			
 		r = result2 + result1;																		// çift ve tek sayıların toplamını r değişkenine atadık.
 		printf("\n\nAradaki sayilarin toplami: %d\n", r);											// aradaki sayıların toplamını ekrana yazdırdık.
-		
-		
+			
+			
 		printf("\nTek sayilarin toplami: %d\n", result1);											// tek sayıların toplamını ekrana yazdırdık.
 		printf("\nTek sayilarin ortalamasi: %.2f\n", average1);										// tek sayıların ortalamasını ekrana yazdırdık.
 		printf("\nCift sayilarin toplami: %d\n", result2);											// çift sayıların toplamını ekrana yazdırdık.
 		printf("\nCift sayilarin ortalamasi: %.2f\n", average2);									// çift sayıların ortalamasını ekrana yazdırdık.
-	}
-	else if(n <= 0 || m <= 0){
-		printf("Lutfen pozitif tam sayi giriniz...");										     	// olası durumlardan birini kontrol etmek için ekrana hata mesajı yazdırdık.
 	}
 	
 	return 0;
