5102881fec4ec3bc80b26fdb86c8c7cf07113cac
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-05-20 22:34:34 +0300

    updated.

--- main.c
+++ main.c
@@ -775,5 +775,121 @@ int main() {
 	
 	
 	
+	
+	
+	/* POINTERLAR (GÖSTERGELER) */
+	
+	
+	// Pointer: Bellekte saklanan deðerin adresini gösterir.
+	
+	/*
+	int i = 5;
+	
+	int *p = &i;		// &: deðiþkenin adres bilgisini veriyor, *: adresteki deðeri veriyor.
+	
+	printf("%u\n", p);
+	
+	printf("%d", *p);
+	*/
+	
+	
+	/*
+	int a = 5, *ap;
+	float b = 3.2, *bp;
+	double c = 3.412, *cp;
+	char d = 'a', *dp;
+	
+	int sayilar[5] = {1, 2, 3, 4, 5};
+	
+	int *arrayp;
+	
+	ap = &a;
+	bp = &b;
+	cp = &c;
+	dp = &d;
+	
+	arrayp = &sayilar[1];
+	
+	printf("%u adresindeki int'in degeri %d'dir.'\n", ap, *ap);
+	printf("%u adresindeki float'in degeri %f'dir.'\n", bp, *bp);
+	printf("%u adresindeki double'in degeri %lf'dir.'\n", cp, *cp);
+	printf("%u adresindeki char'in degeri %c'dir.'\n", dp, *dp);
+	printf("%u adresindeki sayilarin degeri %d'dir.'\n", arrayp, *arrayp);
+	*/
+	
+	
+	
+	/*
+	int i, j = 1;
+	int *jp1, *jp2 = &j;
+	
+	jp1 = jp2;
+	
+	i = ++(*jp2);
+	
+	*jp2 = *jp2 + i;
+	
+	printf("i = %d, j = %d, *jp1 = %d, *jp2 = %d\n", i, j, *jp1, *jp2);
+	*/
+	
+	
+	
+	
+	// CALL BY VALUE
+	
+	/*
+	void  degerDegistir(int a, int b){
+		
+		int gecici = a;
+		a = b;
+		b = gecici;
+		
+		printf("a = %d,  b = %d\n", a, b);
+		
+		
+	}
+	
+	
+	
+	
+	int x=5, y=10;
+	
+	degerDegistir(x, y);
+	
+	printf("x = %d, y = %d", x, y);
+	*/
+	
+	
+	
+	
+	// CALL BY REFERENCE
+	
+	
+	/*
+	void degerDegistir(int *a, int *b) {
+		
+		int gecici = *a;
+		
+		*a = *b;
+		
+		*b = gecici;
+	}
+	
+	
+	
+	int x = 5, y = 10;
+	
+	degerDegistir(&x, &y);
+	
+	printf("x=%d, y=%d", x, y);
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
 	return 0;
 }
