d2930a51b11d7f2c1f30d2c8af4b61bf4a0d31ee
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-05-15 23:11:13 +0300

    created

---
+++ defineKullanimi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define karakter 'a'
+#define sayi 5
+
+int main() {
+  char karakterDizisi[karakter];
+  int sayilar[sayi] = {1, 2, 3, 4, 5};
+  
+  printf("%d\n%c\n", sayi, karakter);
+  return 0;
+}
