ab8f7521b5c4d8db15c552f73eca1116defd6334
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-05-16 17:13:23 +0300

    created

--- karakterDizileriEgzersiz.c
+++ karakterDizileriEgzersiz.c
@@ -7,18 +7,19 @@ int main() {
   // 2 adet karakter dizisi oluştur.
   // 1. "bonne"  2. "soiree"
   // strcpy ve strncpy kullanarak 2 karakter dizisini birleştir
-  // elde ettiğin karakter dizisini ekrana bastır
+  // elde ettiğin karakter dizisi bonsoire olmalı ekrana bastır
 
 
-  char s1[100] = "bonne";
-  char s2[100] = "soiree";
-
-  strcpy(s1, s2);
-  printf("strcpy ile elde edilen dizi: %s\n", s1);
+  char s1[] = "bonne";
+  char s2[] = "soiree";
+  char sonuc[50] = ""; // maks alabileceği karakter dizizisini belirtmemiz gerekli
   
-  strncpy(s2, s1, 2);
-  printf("strtncpy ile elde edilen dizi: %s", s2);
-    
+  strcpy(sonuc, s1);
+  strncpy(sonuc+3, s2, 5);
+  printf("Elde edilen dizi: %s", sonuc);
+
+  
+  return 0;
   
   return 0;
 }
